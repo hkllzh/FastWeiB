a58d3634bd5bb469c726b9991265988d2119e926
lizheng <li396858359@163.com>
2015-10-12 10:40:02 +0800

    修改首页导航布局，使用系统默认样式

--- FastWeiB/src/main/java/com/hkllzh/fastweib/ui/IndexActivity.java
+++ FastWeiB/src/main/java/com/hkllzh/fastweib/ui/IndexActivity.java
@@ -1,15 +1,15 @@
 package com.hkllzh.fastweib.ui;
 
 import android.os.Build;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.hkllzh.android.util.toast.ToastUtil;
 import com.hkllzh.fastweib.FWBBaseActivity;
@@ -17,15 +17,15 @@ import com.hkllzh.fastweib.R;
 
 /**
  * 项目首页
- * <p/>
+ * <p>
  *
  * @author lizheng -- li396858359@163.com
  */
-public class IndexActivity extends FWBBaseActivity {
+public class IndexActivity extends FWBBaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private DrawerLayout drawerLayout;
     private Toolbar toolbar;
-    private FrameLayout left_drawer;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
 
     @Override
     public int getContentViewId() {
@@ -34,23 +34,10 @@ public class IndexActivity extends FWBBaseActivity {
 
     @Override
     protected void initView() {
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        left_drawer = (FrameLayout) findViewById(R.id.left_drawer);
-    }
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
 
-    @Override
-    protected void initData() {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.content_frame, new IndexContentFragment(), "content");
-        ft.add(R.id.left_drawer, new IndexLeftDrawerFragment(), "leftDrawer");
-        ft.commit();
-
-        setToolbar();
-    }
-
-    private void setToolbar() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             toolbar.setTitleTextColor(getResources().getColor(android.R.color.white, null));
@@ -59,16 +46,29 @@ public class IndexActivity extends FWBBaseActivity {
         }
 
         setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(mActivity, drawerLayout, toolbar, R.string.index_drawer_open, R.string.index_drawer_close);
-        drawerToggle.syncState();
-        drawerLayout.setDrawerListener(drawerToggle);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.index_drawer_open, R.string.index_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
     }
 
     @Override
+    protected void initData() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.content_frame, new IndexContentFragment(), "content");
+        // ft.add(R.id.left_drawer, new IndexLeftDrawerFragment(), "leftDrawer");
+        ft.commit();
 
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
@@ -90,7 +90,22 @@ public class IndexActivity extends FWBBaseActivity {
 
     @Override
     protected void setListener() {
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
