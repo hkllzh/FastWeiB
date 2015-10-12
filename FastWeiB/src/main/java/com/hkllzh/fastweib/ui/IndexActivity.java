14efb6dcdf0b8ac84b8fa8091edf57379bbdd39e
lizheng <li396858359@163.com>
2015-10-12 11:10:59 +0800

    按照希望的格式搞定

--- FastWeiB/src/main/java/com/hkllzh/fastweib/ui/IndexActivity.java
+++ FastWeiB/src/main/java/com/hkllzh/fastweib/ui/IndexActivity.java
@@ -96,7 +96,28 @@ public class IndexActivity extends FWBBaseActivity implements NavigationView.OnN
 
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
-        return false;
+        // Handle navigation view item clicks here.
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_camara) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        } else {
+
+        }
+
+        // DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
